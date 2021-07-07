48881cefcb0d008631db5f61744ed7de34745108
jiye <jiyecafe@gmail.com>
2021-07-07 12:19:39 +0800

    test

--- config.example.js
+++ config.example.js
@@ -1,5 +1,5 @@
 // 服务模式[开发-dev? | 测试-test | 上线-prod]
-const MODE = 'dev1';
+const MODE = 'test';
 
 // RSA公钥，用于加密密码
 const RSAPubKey = `YOUR_PUBLIC_RSA_KEY`
@@ -19,7 +19,7 @@ const API_DOMAIN = {
     dev1: 'http://192.168.1.232/api',
     dev2: 'http://127.0.0.1/api',
     dev3: 'http://127.0.0.1:8888/Api',
-    test: 'https://test.cuit.api.jysafe.cn/api',
+    test: 'https://test.cuit.api.jysafe.cn/Api',
     prod: 'https://cuit.api.jysafe.cn/api'
 };
 
